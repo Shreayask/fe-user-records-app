b376763fe740a9fae88b8370141e8f7ec670e18b
Shreayask <shreayasinghk@gmail.com>
2024-10-15 16:37:48 +0545

    feature: added login and signup page

---
+++ script/login.js
@@ -0,0 +1,76 @@
+$(document).ready(function () {
+
+
+    $('.login-button').on('click', function () {
+      // Get values from input fields
+      const username = $('#username').val();
+      const password = $('#password').val();
+
+
+      if (!username || !password) {
+        $('#error-msg').text('All fields are required.')
+        return;
+      }
+
+      if (password.length < 8) {
+        $('#error-msg').text('Password must be at least 8 characters long.')
+        return;
+      }
+      const params = {
+        username,
+        password,
+
+      };
+
+
+      // Making the API request using axios
+      axios.get('http://localhost:3000/login', { params })
+        .then(function (response) {
+          $('#error-msg').text('')
+
+          const loggedinUser = response.data.data
+         localStorage.setItem('userId',loggedinUser._id)
+          // Set modal attributes to toast msg
+          const toast = $('#liveToast');
+          toast.addClass('toast fade show');
+          setTimeout(function () {
+            // Navigate to dashboard
+            if (loggedinUser.admin) {
+              window.location.href = './adminDashboard.html';
+            } else {
+              window.location.href = './userProfile.html';
+            }
+
+          }, 2000);
+
+
+        })
+        .catch(function (error) {
+          console.error('Error:', error);
+          $('#error-msg').text(`${error.response.data.message || 'Login Failed'}`)
+
+        });
+    });
+
+    $('#login-button').click(function () {
+      window.location.href = './login.html'
+    })
+    $('#close-toast').click(function () {
+      const toast = $('#liveToast');
+
+      // Set modal attributes
+      toast.removeClass('fade show');
+    })
+    //close modal
+    $('#close-modal').click(function () {
+      const modal = $('#signupSuccessModal');
+
+      // Set modal attributes
+      modal.addClass('modal fade ');
+      modal.css('display', 'none');
+      modal.removeAttr('aria-modal');
+    })
+  }
+
+
+  );
---
+++ script/signup.js
@@ -0,0 +1,71 @@
+$(document).ready(function () {
+
+
+    $('.signup-button').on('click', function () {
+        // Get values from input fields
+        const name = $('#name').val();
+        const email = $('#email').val();
+        const number = $('#phonenumber').val();
+        const address = $('#address').val();
+        const username = $('#username').val();
+        const password = $('#password').val();
+        const gender = $('input[name="gender"]:checked').val();
+
+        //validation for user form
+        if (!name || !email || !number || !address || !username || !password || !gender) {
+           $('#error-msg').text('All fields are required.')
+            return; 
+        }
+
+        if (password.length < 8) {
+            $('#error-msg').text('Password must be at least 8 characters long.')
+            return; 
+        }
+        const userData = {
+            name,
+            number,
+            email,
+            address,
+            username,
+            password,
+            gender
+        };
+
+
+        // Making the API request using axios
+        axios.post('http://localhost:3000/signup', userData)
+            .then(function (response) {
+                console.log('Success:', response.data, $('#signupSuccessModal'));
+                $('#error-msg').text('')
+
+                const modal = $('#signupSuccessModal');
+
+                // Set modal attributes and display popup
+                modal.addClass('modal fade show');
+                modal.css('display', 'block');
+                modal.attr('aria-modal', 'true');
+            })
+            .catch(function (error) {
+                $('#error-msg').text(error.response.data.message)
+                console.error('Error:', error);
+                
+            });
+    });
+
+    $('#login-button').click(function () {
+        window.location.href = './login.html'
+    })
+   
+    //close modal
+    $('#close-modal').click(function () {
+        const modal = $('#signupSuccessModal');
+
+        // Set modal attributes
+        modal.addClass('modal fade ');
+        modal.css('display', 'none');
+        modal.removeAttr('aria-modal');
+    })
+}
+
+
+);
